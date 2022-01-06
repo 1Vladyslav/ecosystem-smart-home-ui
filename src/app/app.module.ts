31e4735f6820f057098caa5a243a8a6f5cdc272a
1Vladyslav <tel19082018@gmail.com>
2022-01-06 03:34:04 +0200

    -first page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RecommendationsComponent } from './recommendations/recommendations.component';
+import { RoomInfoComponent } from './room-info/room-info.component';
+
+const appRoutes: Routes =[
+  { path: 'recomendations', component: RecommendationsComponent},
+
+];
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot([
+      {path: 'recomendations', component: RecommendationsComponent}
+    ]),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [ AppComponent, RecommendationsComponent, RoomInfoComponent],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
